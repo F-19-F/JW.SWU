fa353711871aa87569c4a54a406e204d048673db
F-19-F <58457605+F-19-F@users.noreply.github.com>
2021-07-08 23:05:14 +0800

    更新支持二级菜单选课

--- Course_Selection.py
+++ Course_Selection.py
@@ -169,25 +169,80 @@ class CS:
         res = self.session.post(url='http://{}/jwglxt/xsxk/zzxkyzbjk_cxJxbWithKchZzxkYzb.html?gnmkdm=N253512&su={}'.format(
             HOST, self.num), data=post_params, headers=post_headers)
         target = res.json()[0]
+        do_jxb_id=target['do_jxb_id']#do_jxb_id,这个参数比较特殊
+        #有子课程的课程
+        if course_info['jxbzls'] == '2':#jxb_ids=target['do_jxb_id']+'%2C'+实践课jxb_id
+            html_api='http://jw.swu.edu.cn/jwglxt/xsxk/zzxkyzb_xkZyZzxkYzbZjxb.html?jxb_id={jxb_id}&do_jxb_id={do_jxb_id}&jxbzls={jxbzls}&rlkz={rlkz}&fxbj={fxbj}&cxbj={cxbj}&rlzlkz={rlzlkz}&rwlx={rwlx}&syqz={syqz}&time={time}&gnmkdm=N253512&su={num}'.format(
+                jxb_id=course_info['jxb_id'],
+                do_jxb_id=target['do_jxb_id'],
+                jxbzls='2',
+                rlkz=self.params['rlkz'],
+                fxbj=course_info['fxbj'],
+                cxbj=course_info['cxbj'],
+                rlzlkz=self.params['rlzlkz'],
+                rwlx=self.params['rwlx'],
+                syqz='100',
+                time=str(int(time.time()*1000)),
+                num=self.num
+            )
+            h_headers = headers
+            h_headers['X-Requested-With'] = 'XMLHttpRequest'
+            h_headers['Origin'] = HOST
+            h_headers['Accept'] = 'text/html, */*; q=0.01'
+            h_headers['Referer'] = 'http://{}/jwglxt/xsxk/zzxkyzb_cxZzxkYzbIndex.html?gnmkdm=N253512&layout=default&su={}'.format(
+                HOST, self.num)
+            self.session.post(url=html_api,headers=h_headers)
+            post_params={
+                'xkxnm':self.params['xkxnm'],
+                'xkxqm':self.params['xkxqm'],
+                'xkly':self.params['xkly'],
+                'jxb_id':target['do_jxb_id'],
+                'jxbzls':course_info['jxbzls'],
+                'rlkz':self.params['rlkz'],
+                'rlzlkz':self.params['rlzlkz'],
+                'rwlx':self.params['rwlx'],
+                'syqz':'100',#默认100
+                'zyfx_id':self.params['zyfx_id'],
+                'bh_id':self.params['bh_id'],
+                'zyh_id':self.params['zyh_id'],
+                'njdm_id':self.params['njdm_id'],
+                'sfkknj':self.params['sfkknj'],
+                'sfkkzy':self.params['sfkkzy'],
+                'zh':'',
+                'sfznkx':self.params['sfznkx'],
+                'kklxdm':course_info['kklxdm'],
+                'xh_id':self.num,
+                'bklx_id':self.params['bklx_id'],
+                'kkbk':self.params['kkbk'],
+                'kkbkdj':self.params['kkbkdj'],
+                'fxbj':course_info['fxbj'],
+                'cxbj':course_info['cxbj']
+            }
+            res=self.session.post(url='http://{}/jwglxt/xsxk/zzxkyzb_xkZyDisplayZzxkYzbZjxb.html?gnmkdm=N253512&su={}'.format(HOST,self.num),data=post_params,headers=post_headers)
+            res=res.json()
+            if len(res)>1:
+                t=res[1]
+            do_jxb_id+='%2C'
+            do_jxb_id+=t['do_jxb_id']
         #准备参数，进行最终选课请求的发送
         post_params = {
-            'jxb_ids': target['do_jxb_id'],
-            'kch_id': course_info['kch_id'],
-            'kcmc': "({}){}+-+{}+学分".format(course_info['kch'], course_info['kcmc'], course_info['xf']),
-            'rwlx': self.params['rwlx'],
-            'rlkz': self.params['rlkz'],
-            'rlzlkz': self.params['rlzlkz'],
+            'jxb_ids': do_jxb_id,
+            'kch_id': course_info['kch_id'],#课程相关参数
+            'kcmc': "({}){}+-+{}+学分".format(course_info['kch'], course_info['kcmc'], course_info['xf']),#不变
+            'rwlx': self.params['rwlx'],#全局参数
+            'rlkz': self.params['rlkz'],#全局参数
+            'rlzlkz': self.params['rlzlkz'],#全局参数
             'sxbj': '1',  # 固定值
             'xxkbj': course_info['xxkbj'],
             'qz': '0',  # 固定值
             'cxbj': course_info['cxbj'],
             'xkkz_id': self.params['firstXkkzId'],
-            'njdm_id': self.params['njdm_id'],
+            'njdm_id': self.params['njdm_id'],#全局参数
             'zyh_id': self.params['zyh_id'],
-            'kklxdm': course_info['kklxdm'],
-            'xklc': self.params['xklc'],
-            'xkxnm': self.params['xkxnm'],
-            'xkxqm': self.params['xkxqm'],
+            'kklxdm': course_info['kklxdm'],#全局参数
+            'xklc': self.params['xklc'],#选课轮次
+            'xkxnm': self.params['xkxnm'],#选课学年名
+            'xkxqm': self.params['xkxqm'],#选课学期名
         }
         res = self.session.post(url='http://{}/jwglxt/xsxk/zzxkyzbjk_xkBcZyZzxkYzb.html?gnmkdm=N253512&su={}'.format(
             HOST, self.num), data=post_params, headers=post_headers)
